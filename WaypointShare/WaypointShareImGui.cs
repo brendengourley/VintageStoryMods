3f2df756306a89a07d11ace8741ea95bd37d600a
Brenden Gourley <brendengourley@gmail.com>
2025-12-05 11:46:28 -0700

    Fix some more errs

--- WaypointShare/WaypointShareImGui.cs
+++ WaypointShare/WaypointShareImGui.cs
@@ -69,7 +69,7 @@ namespace WaypointShare
             // Set up chat message listener to capture waypoint list output
             bool isListening = false;
             
-            ClientChatLineDelegate chatHandler = (int groupId, string message, EnumChatType chattype, string data) =>
+            ClientChatLineDelegate chatHandler = (message, chattype, data) =>
             {
                 if (isListening && chattype == EnumChatType.CommandSuccess)
                 {
